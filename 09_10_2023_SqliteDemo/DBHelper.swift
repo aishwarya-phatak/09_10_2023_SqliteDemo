d5fb2e345f8f37e5bc749a21dde5bfdd9510f5cd
aishwarya <ashp17star@gmail.com>
2024-01-04 13:22:08 +0530

    CRUD Operation on sqlite

--- 09_10_2023_SqliteDemo/DBHelper.swift
+++ 09_10_2023_SqliteDemo/DBHelper.swift
@@ -68,15 +68,17 @@ class DBHelper{
             -1,
             &insertStatement,
             nil) == SQLITE_OK{
-            if sqlite3_step(insertStatement) == SQLITE_DONE{
-                sqlite3_bind_int(insertStatement,0, Int32(empId))
-                sqlite3_bind_text(insertStatement,
-                                  1,
+            sqlite3_bind_int(insertStatement,1, Int32(empId))
+            sqlite3_bind_text(insertStatement,
+                                  2,
                                   (empName as NSString).utf8String,
                                   -1,
                                   nil)
-            }else{
-                print("Employee Record successfully inserted")
+            //to check whether row is inserted or not
+            if sqlite3_step(insertStatement) == SQLITE_DONE{
+                print("Successful execution")
+            } else {
+                print("Unsuccessful execution")
             }
         }else{
             print("Insert Statement Preparation has failed")
@@ -85,7 +87,49 @@ class DBHelper{
         sqlite3_finalize(insertStatement)
     }
     
-//    func deleteAnEmployeeRecord(){
-//
-//    }
+    func deleteAnEmployeeRecord(empId : Int){
+        let deleteQUeryString = "DELETE FROM Employee where empId = ?;"
+        var deleteStatement : OpaquePointer?
+        if sqlite3_prepare_v2(
+            db,
+            deleteQUeryString,
+            -1,
+            &deleteStatement,
+            nil) == SQLITE_OK{
+            sqlite3_bind_int(deleteStatement, 1, Int32(empId))
+        } else {
+            print("Delete statement preparation failed")
+        }
+        sqlite3_finalize(deleteStatement)
+    }
+    
+    func retriveEmployeeRecords()->[Employee]{
+        var employees :[Employee] = []
+        let retriveEmployeeQueryString = "SELECT * FROM Employee;"
+        var retirveStatement : OpaquePointer?
+        
+        if sqlite3_prepare_v2(
+            db,
+            retriveEmployeeQueryString,
+            -1,
+            &retirveStatement,
+            nil) == SQLITE_OK{
+            while sqlite3_step(retirveStatement) == SQLITE_ROW{
+               let retrivedEmpId =  (sqlite3_column_int(retirveStatement, 0))
+            
+               let retrivedEmpName =
+                String(describing: String(cString:sqlite3_column_text(retirveStatement, 1)))
+                print(retrivedEmpName)
+            
+               let retrivedEmployee =
+                Employee(
+                    empId: Int(retrivedEmpId),
+                    empName: retrivedEmpName)
+                employees.append(retrivedEmployee)
+            }
+        } else {
+            print("Statement Preparation failed")
+        }
+        return employees
+    }
 }
--- 09_10_2023_SqliteDemo/ViewController.swift
+++ 09_10_2023_SqliteDemo/ViewController.swift
@@ -9,9 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var employees : [Employee] = []
     override func viewDidLoad() {
         super.viewDidLoad()
        var dbHelper = DBHelper()
         dbHelper.insertAnEmployeeRecord(empId: 11, empName: "Gajanan")
+        dbHelper.insertAnEmployeeRecord(empId: 12, empName: "Ashutosh")
+        dbHelper.insertAnEmployeeRecord(empId: 13, empName: "Rutik")
+        
+        print("-------------")
+        
+        employees = dbHelper.retriveEmployeeRecords()
+        for eachEmployee in employees{
+            print("\(eachEmployee.empId) --- \(eachEmployee.empName)")
+        }
+        
+//        print("-------------")
+//        
+//        dbHelper.deleteAnEmployeeRecord(empId: 13)
+//        
+//        print("-------------")
+//        
+//        employees = dbHelper.retriveEmployeeRecords()
+//        for eachEmployee in employees{
+//            print("\(eachEmployee.empId) --- \(eachEmployee.empName)")
+//        }
+        
     }
 }
